3f7f753266d6707b365fb508ee42129fc44aeee5
ArthurM2 <38484496+ArthurM2@users.noreply.github.com>
2023-06-12 01:56:56 +0500

    Переписал на усложненный вариант: - Добавил +- кнопки для добавления и удаления в каунтере - Добавил кнопку ресет до 0 - Добавил вывод даты в текст вью - Добавил текст вью - Добавил проверну на отрицательное число

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,20 +10,42 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
+    @IBOutlet weak var infoTextView: UITextView!
     
     var count = 0
+    let date = Date().formatted(date: .abbreviated, time: .standard)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func didButtonTap(_ sender: Any) {
-        count += 1
-        counterLabel.text = "\(count)"
-        print("You tapped on button \(count) times")
-        
+        infoTextView.text = "История изменений:\n"
+        infoTextView.isEditable = false
+        infoTextView.isScrollEnabled = true
     }
     
+    @IBAction func didIncrementButton(_ sender: Any) {
+        count += 1
+        counterLabel.text = "\(count)"
+        infoTextView.text += "[\(date)]: +1\n"
+    }
+    
+    @IBAction func resetCounterButton(_ sender: Any) {
+        count = 0
+        counterLabel.text = "\(count)"
+        infoTextView.text += "[\(date)]: значение сброшено \n"
+    }
+    
+    @IBAction func didDecrementButton(_ sender: Any) {
+        count -= 1
+
+        if count <= 0 {
+            count = 0
+            infoTextView.text += "[\(date)]: попытка уменьшить значение счётчика ниже 0\n"
+        }
+        counterLabel.text = "\(count)"
+        infoTextView.text += "[\(date)]: -1\n"
+    }
 }
 
