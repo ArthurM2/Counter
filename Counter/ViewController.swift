a6577657183ab9fc287734feea42efb5422c0674
ArthurM2 <38484496+ArthurM2@users.noreply.github.com>
2023-06-13 13:09:31 +0500

    - Добавил приватный модификатор функциям и переменным

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,34 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var decrementButton: UIButton!
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var restartButton: UIButton!
-    @IBOutlet weak var infoTextView: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var decrementButton: UIButton!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var restartButton: UIButton!
+    @IBOutlet private weak var infoTextView: UITextView!
     
-    var count = 0
+    private var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         infoTextView.text = "История изменений:\n"
         infoTextView.isEditable = false
         infoTextView.isScrollEnabled = true
     }
     
-    @IBAction func didIncrementButton(_ sender: Any) {
+    @IBAction private func didIncrementButton(_ sender: Any) {
         count += 1
         counterLabel.text = "\(count)"
         infoTextView.text += "[\(Date().formatted(date: .abbreviated, time: .standard))]: +1\n"
     }
     
-    @IBAction func resetCounterButton(_ sender: Any) {
+    @IBAction private func resetCounterButton(_ sender: Any) {
         count = 0
         counterLabel.text = "\(count)"
         infoTextView.text += "[\(Date().formatted(date: .abbreviated, time: .standard))]: значение сброшено \n"
     }
     
-    @IBAction func didDecrementButton(_ sender: Any) {
+    @IBAction private func didDecrementButton(_ sender: Any) {
         if count <= 0 {
             count = 0
             infoTextView.text += "[\(Date().formatted(date: .abbreviated, time: .standard))]: попытка уменьшить значение счётчика ниже 0\n"
